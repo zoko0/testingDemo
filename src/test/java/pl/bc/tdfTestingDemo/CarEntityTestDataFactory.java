ff171bfa8fb861a21ae1e5a45abccdc8bbf3c84e
bartosz.ciulkin <bartosz.ciulkin@bshg.com>
2021-09-04 13:21:30 +0200

    Introduce TDF

---
+++ src/test/java/pl/bc/tdfTestingDemo/CarEntityTestDataFactory.java
@@ -0,0 +1,36 @@
+package pl.bc.tdfTestingDemo;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import pl.bc.tdfTestingDemo.model.CarEntity;
+import pl.bc.tdfTestingDemo.model.CarEntity.CarEntityBuilder;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CarEntityTestDataFactory {
+
+  public static CarEntity standardCarEntity() {
+
+    return standardCarEntityBuilder().build();
+  }
+
+  private static CarEntityBuilder standardCarEntityBuilder() {
+
+    return CarEntity.builder()
+        .carId("carId")
+        .brand("WW")
+        .model("Golf")
+        .description("description2")
+        .isElectric(false)
+        .trunkCapacity(15)
+        .milesTravelled(200500)
+        .driver("Jan Andrzej")
+        .year(2012);
+  }
+
+  public static CarEntity createCarWithYear(int year) {
+
+    return standardCarEntityBuilder()
+        .year(year)
+        .build();
+  }
+}
